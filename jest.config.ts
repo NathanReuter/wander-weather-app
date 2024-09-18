251017bb143a2c4e2dac1b7de0a57dc2a3c51602
nathan reuter <nathan0reuter@gmail.com>
2024-09-18 11:53:35 -0300

    adding project config changes

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,14 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+    transform: {
+        '^.+\\.ts$': 'ts-jest'
+    },
+    testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
+    globals: {
+        'ts-jest': {
+            tsconfig: 'tsconfig.json'
+        }
+    }
 };
